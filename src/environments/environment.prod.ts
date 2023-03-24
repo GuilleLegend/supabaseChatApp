a06187c78517aa4a0b7b72add1f39efc2d3c40ae
Guille <ghervas88@gmail.com>
2023-03-24 13:05:49 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://kuqehucherjmdiclrgaa.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1cWVodWNoZXJqbWRpY2xyZ2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk2NTc1MzQsImV4cCI6MTk5NTIzMzUzNH0.O-9ldBeLSvmRjJBCCOrAhO6CZNnLTxAyfSy-iX0iUW0',
+};
